b3258d42721dd0eedf10b50979a567b75e252833
traguill <traguill1@gmail.com>
2018-03-26 22:05:01 +0200

    Editor Server base

--- BeeT/BeeTGUI/Network.cpp
+++ BeeT/BeeTGUI/Network.cpp
@@ -2,7 +2,6 @@
 #include "Log.h"
 
 #include "../SDL2/include/SDL.h"
-#include "../SDL2/include/SDL_net.h"
 
 Network::Network(const char* name) : Module(name)
 {}
@@ -19,12 +18,147 @@ bool Network::Init()
 		return false;
 	}
 	LOGI("SDLNet initialized");
+
+	// Create a socket set
+	socketSet = SDLNet_AllocSocketSet(maxSockets);
+	if (socketSet == NULL)
+	{
+		LOGE("SDLNet create socket set: %s", SDLNet_GetError());
+		return false;
+	}
+
+	// Initialize all the clients sockets
+	maxClients = maxSockets - 1;
+	clientSocket.resize(maxClients);
+	std::fill(clientSocket.begin(), clientSocket.end(), nullptr);
+	freeSockets.resize(maxClients);
+	std::fill(freeSockets.begin(), freeSockets.end(), true);
+
+	
+	const unsigned short port = 8080;
+	int hostResolved = SDLNet_ResolveHost(&serverIP, NULL, port);
+	if (hostResolved == -1)
+	{
+		LOGE("SDLNet resolve host: %s", SDLNet_GetError());
+		return false;
+	}
+
+	msgBuffer = new char[MESSAGE_MAX_SIZE];
+	ListenConnections();
+
 	return true;
 }
 
 bool Network::CleanUp()
 {
 	LOGI("SDLNet quitting");
+
+	SDLNet_FreeSocketSet(socketSet);
+	if (listening)
+		SDLNet_TCP_Close(serverSocket);
+
+	if (msgBuffer)
+		delete[] msgBuffer;
 	SDLNet_Quit();
 	return true;
+}
+
+bool Network::Update()
+{
+	if (listening)
+	{
+		HandleNewConnections();
+		HandleClientConnections();
+	}
+	return true;
+}
+
+bool Network::ListenConnections()
+{
+	if (!listening)
+	{
+		// Open the server socket
+		serverSocket = SDLNet_TCP_Open(&serverIP);
+		if (!serverSocket)
+		{
+			LOGE("SDLNet failed to open Server socket %s", SDLNet_GetError());
+		}
+		else
+		{
+			SDLNet_TCP_AddSocket(socketSet, serverSocket);
+			listening = true;
+		}
+	}
+	return listening;
+}
+
+void Network::StopListeningConnections()
+{
+	if (listening)
+	{
+		SDLNet_TCP_DelSocket(socketSet, serverSocket);
+		SDLNet_TCP_Close(serverSocket);
+		serverSocket = NULL;
+		listening = false;
+	}
+}
+
+void Network::HandleNewConnections()
+{
+	int numActiveSockets = SDLNet_CheckSockets(socketSet, 0);
+
+	int serverSocketReady = SDLNet_SocketReady(serverSocket);
+
+	if (serverSocketReady != 0)
+	{
+		auto freeSpotIt = std::find(freeSockets.begin(), freeSockets.end(), true);
+		*freeSpotIt = false;
+		unsigned int index = freeSpotIt - freeSockets.begin();
+		
+		clientSocket[index] = SDLNet_TCP_Accept(serverSocket);
+		
+		SDLNet_TCP_AddSocket(socketSet, clientSocket[index]);
+		numClientsConnected++;
+		//Send Acknowledgement for connection
+
+		//SDLNet_TCP_Send(clientSocket[index], (void*)buffer, length);
+	}
+	else // No space for new Clients
+	{
+		LOGW("Network: Maximum number of clients connected. Rejecting connection");
+
+		TCPsocket tmpSocket = SDLNet_TCP_Accept(serverSocket); // Accept to clear it from incoming connections
+		// Send msg refusing connection
+		//SDLNet_TCP_Send(tmpSocket, (void*)buffer, length);
+
+		SDLNet_TCP_Close(tmpSocket);
+	}
+}
+
+void Network::HandleClientConnections()
+{
+	for (int id = 0; id < maxClients; id++)
+	{
+		int clientSocketReady = SDLNet_SocketReady(clientSocket[id]);
+		if (clientSocketReady != 0)
+		{
+
+			int dataRecv = SDLNet_TCP_Recv(clientSocket[id], msgBuffer, MESSAGE_MAX_SIZE);
+
+			// Either the connection was closed or an error occurred
+			if (dataRecv <= 0)
+			{
+				SDLNet_TCP_DelSocket(socketSet, clientSocket[id]);
+				SDLNet_TCP_Close(clientSocket[id]);
+				clientSocket[id] = nullptr;
+				freeSockets[id] = true;
+				numClientsConnected--;
+			}
+			else // Data has been read successfully
+			{
+				// Send recv ack
+				//SDLNet_TCP_Send(clientSocket[id], (void*)buffer, length);
+			}
+		}
+	}
 }
--- BeeT/BeeTGUI/Network.h
+++ BeeT/BeeTGUI/Network.h
@@ -2,6 +2,10 @@
 #define __NETWORK_H__
 
 #include "Module.h"
+#include "../SDL2/include/SDL_net.h"
+#include <vector>
+
+#define MESSAGE_MAX_SIZE 512 // TODO: Change this
 
 class Network : public Module
 {
@@ -11,8 +15,30 @@ public:
 
 	bool Init();
 	bool CleanUp();
+	bool Update();
+
+	bool ListenConnections();
+	void StopListeningConnections();
 
 private:
+
+	void HandleNewConnections();
+	void HandleClientConnections();
+
+private:
+	IPaddress serverIP;
+
+	unsigned short maxSockets = 4;
+	unsigned short maxClients;
+	std::vector<TCPsocket> clientSocket;
+	std::vector<bool> freeSockets;
+	SDLNet_SocketSet socketSet;
+	TCPsocket serverSocket;
+
+	bool listening = false;
+	int numClientsConnected = 0;
+
+	char* msgBuffer = nullptr; // Buffer to read incomming messages
 
 };
 #endif // !__NETWORK_H__
